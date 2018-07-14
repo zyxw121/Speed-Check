1c255774e9dca99f08869b4096fbf10cd6b9e9cf
Daniel Mroz <daniel.mroz@univ.ox.ac.uk>
2018-07-14 21:41:03 +0100

    worked on structure

---
+++ client.rkt
@@ -0,0 +1,5 @@
+#lang racket
+(require racket/tcp)
+(require "speedcheck.rkt")
+
+(speedcheck-client)
--- speedcheck.rkt
+++ speedcheck.rkt
@@ -1,5 +1,5 @@
 #lang racket
-
+ 
 (provide
  speedcheck-server
  speedcheck-client)
@@ -8,55 +8,55 @@
 
 ;serve a client with the given channels
 (define (serve in out)
-  (match (read-command in)
-    ['Download (handle-download in out)
+  (define x (read-byte in))
+  (displayln x)
+  (match x
+    [0
+     (print "download req")
+     (write-byte 10 out)               
                (serve in out)]
-    ['Upload (handle-upload in out)
+    [1 (handle-upload in out)
              (serve in out)]
-    ['Quit null]))
-
-(define (read-command in)
-  (match (read-byte in)
-    [0 'Download]
-    [1 'Upload]
-    [2 'Quit]))
-
-(define (send-command cmd out)
-  (write-byte
-   (match cmd
-    ['Download 0]
-    ['Upload 1]
-    ['Quit 2])
-   out))
+    [_ null])
+)
+; have to handle eof somehow
 
 (define (handle-download in out)
   (for/list ([i (in-range (read-byte in))])
-    (write-bytes (make-bytes (* 1000 1000) 0) out))
-  (flush-output))
+    (write-bytes (make-bytes (* 1000 1000) 0) out)
+    (flush-output)))
 
 (define (handle-upload in out)
   (for/list ([i (in-range (read-byte in))])
     (read-bytes (* 1000 1000) in)))
 
 (define (request-upload n in out)
-  (send-command 'Upload out)
+;  (send-command 'Upload out)
   (for/list ([i (in-range n)])
     (write-bytes (make-bytes (* 1000 1000) 0) in)))
 
 (define (request-download n in out)
-  (send-command 'Download out)
+;  (send-command 'Download out)
+  (write-bytes (make-bytes 1 n) out)
+  (flush-output out)
   (for/list ([i (in-range n)])
     (read-bytes (* 1000 1000) in)))
 
 (define (speedcheck-server)
   (define listener (tcp-listen 8081))
-  (print "server started")
-  (let-values ([(in out) (tcp-accept listener)])
-    (print "server connected")
-    (serve in out)
-    (close-input-port in)
-    (close-output-port out))
-  (speedcheck-server))
+  (define (loop) 
+    (let-values ([(in out) (tcp-accept listener)])
+      (print "client connected")
+      (serve in out)
+      ;(displayln (read-byte in))
+      ;(write-string "Hello" out)
+      ;(flush-output out)
+      (close-input-port in)
+      (close-output-port out)
+      (print "client gone"))
+    (loop))
+  (print "started")
+  (loop))
 
 (define (with-time f)
   (define-values (a b c d) (time-apply f null))
@@ -66,7 +66,6 @@
 (define (speedcheck-client)
   (define-values (in out) (tcp-connect "localhost" 8081))
   (displayln "connected")
-  (map (lambda x (with-time (request-download 10 in out))) (sequence->list (in-range 10)))
+  (with-time (lambda () (request-download 10 in out)))
   (close-input-port in)
   (close-output-port out))
-
