7372a246662cd8db7de13b7177d054356a07f476
Daniel Mroz <daniel.mroz@univ.ox.ac.uk>
2018-07-18 21:24:51 +0100

    mostly working base

--- speedcheck.rkt
+++ speedcheck.rkt
@@ -8,12 +8,9 @@
 
 ;serve a client with the given channels
 (define (serve in out)
-  (define x (read-byte in))
-  (displayln x)
-  (match x
-    [0
-     (print "download req")
-     (write-byte 10 out)               
+  (define cmd (read-byte in))
+  (match cmd
+    [0 (handle-download in out)               
                (serve in out)]
     [1 (handle-upload in out)
              (serve in out)]
@@ -31,12 +28,11 @@
     (read-bytes (* 1000 1000) in)))
 
 (define (request-upload n in out)
-;  (send-command 'Upload out)
   (for/list ([i (in-range n)])
     (write-bytes (make-bytes (* 1000 1000) 0) in)))
 
 (define (request-download n in out)
-;  (send-command 'Download out)
+  (write-bytes (make-bytes 1 0) out)
   (write-bytes (make-bytes 1 n) out)
   (flush-output out)
   (for/list ([i (in-range n)])
@@ -46,26 +42,18 @@
   (define listener (tcp-listen 8081))
   (define (loop) 
     (let-values ([(in out) (tcp-accept listener)])
-      (print "client connected")
       (serve in out)
-      ;(displayln (read-byte in))
-      ;(write-string "Hello" out)
-      ;(flush-output out)
       (close-input-port in)
-      (close-output-port out)
-      (print "client gone"))
+      (close-output-port out))
     (loop))
-  (print "started")
   (loop))
 
 (define (with-time f)
   (define-values (a b c d) (time-apply f null))
-  (print b)
   b)
 
 (define (speedcheck-client)
   (define-values (in out) (tcp-connect "localhost" 8081))
-  (displayln "connected")
-  (with-time (lambda () (request-download 10 in out)))
+  (with-time (lambda () (request-download 100 in out)))
   (close-input-port in)
   (close-output-port out))
