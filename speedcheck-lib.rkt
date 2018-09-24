435134b7897d735f56938daf3718e7533079d93a
Daniel Mroz <daniel.mroz@univ.ox.ac.uk>
2018-09-24 13:07:24 +0100

    made prompt better, upgraded control flow

--- speedcheck-lib.rkt
+++ speedcheck-lib.rkt
@@ -6,7 +6,7 @@
 
 (require racket/tcp)
 
-;serve ad client with the given channels
+;serve a client with the given channels
 (define (serve in out)
   (define cmd (read-byte in))
   (match cmd
@@ -27,7 +27,10 @@
   (for/list ([i (in-range (read-byte in))])
     (read-bytes (* 1000 1000) in)))
 
+; 0 <= n < 256
 (define (request-upload n in out)
+  (write-bytes (make-bytes 1 1) out)
+  (write-bytes (make-bytes 1 n) out)
   (for/list ([i (in-range n)])
     (write-bytes (make-bytes (* 1000 1000) 0) out)))
 
@@ -52,7 +55,9 @@
   (define-values (a b c d) (time-apply f null))
   b)
 
-(define (connect-loop in out)
+(define (connect-loop name disconnect in out)
+  (display name)
+  (display ">")
   (define cmd (read-line))
   (match cmd
     [(? (lambda (x) (regexp-match-positions #rx"up .*" x))  (app (lambda (x) (string->number (substring x 3))) (? number? n) )) 
@@ -61,17 +66,19 @@
                        (display "ms   ")
                        (display (exact->inexact (/ (round (/ (* 100000 n) t)) 100))) ;horrible
                        (displayln "MBps")
-                       (connect-loop in out)]    
+                       (connect-loop name disconnect in out)]    
     [(? (lambda (x) (regexp-match-positions #rx"down .*" x))  (app (lambda (x) (string->number (substring x 5))) (? number? n) ))
                        (define t (with-time (lambda () (request-download n in out))))
                        (display t)
                        (display "ms   ")
                        (display (exact->inexact (/ (round (/ (* 100000 n) t)) 100))) ;horrible
                        (displayln "MBps")
-                       (connect-loop in out)]
+                       (connect-loop name disconnect in out)]
     [":d" (displayln "Disconnected from server")
                   (close-input-port in)
-                  (close-output-port out)]
+                  (close-output-port out)
+                  (disconnect)]
+    [":q" (displayln "Quitting")]
     [":?" (displayln "help")
           (connect-loop in out)]
     [_ (displayln "Bad input. ;? for help")])
@@ -84,13 +91,11 @@
   (match cmd
     [(? (lambda (x) (regexp-match-positions #rx"connect .*" x))  _) (define-values (in out) (tcp-connect (substring cmd 8) 8080))
                        (displayln (string-append "Connected to server " (substring cmd 8) ))
-                       (connect-loop in out)                                            
-                       (loop)]
+                       (connect-loop (substring cmd 8) loop in out)]
     [":q" (displayln "Quitting")]
     [":?" (displayln "help")
           (loop)]
     [_ (displayln "Bad input. :? for help")
        (loop)]))
   (displayln "Speedcheck, version 0.0.1: https://github.com/zyxw121/Speed-Check   :? for help")
-
   (loop))
