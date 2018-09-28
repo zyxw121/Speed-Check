2ef8aebd238b9fa436e15eb2a21addcee42f937c
Daniel Mroz <daniel.mroz@univ.ox.ac.uk>
2018-09-28 16:50:46 +0100

    Added percentage reporting

--- speedcheck-server.rkt
+++ speedcheck-server.rkt
@@ -6,36 +6,27 @@
     (write-bytes (make-bytes (* 1000 n) 0) out)
     (flush-output))
 
-
 (define (handle-upload in out)
   (define n (string->number (read-line in)))
-  (read-bytes (* 1000 n) in))
+  (read-bytes (* 1000 n) in)
+  void)
 
-
-(define (start-serve listener)
+(define (serve listener)
   (let-values ([(in out) (tcp-accept listener)])
-      (serve in out)
+    (thread (lambda () 
+    (match (read-byte in)
+    [0 (handle-download in out)]
+    [1 (handle-upload in out)]
+    [_ void])
       (close-input-port in)
-      (close-output-port out)))
+      (close-output-port out)))))
 
 ;serve a client with the given channels
-(define (serve in out)
-  (define cmd (read-byte in))
-  (match cmd
-    [0 (handle-download in out)
-               (serve in out)]
-    [1 (handle-upload in out)
-             (serve in out)]
-    [_ null]))
-
-
 (define (speedcheck-server)
   (define listener (tcp-listen 8080))
-  (define (my-thread) (start-serve listener))
   (define (loop)  
-    (if (tcp-accept-ready? listener) (start-serve listener) null)
+    (serve listener) 
     (loop))
   (loop))
-
 
 (speedcheck-server)
--- speedcheck.rkt
+++ speedcheck.rkt
@@ -10,8 +10,7 @@
   (newline out)
   (flush-output out)
   (define (upload)
-      (write-bytes (make-bytes (* 1000 n) 6) out)
-    )
+      (write-bytes (make-bytes (* 1000 n) 6) out))
   (with-time upload))
 
 ; n : int, kB
@@ -29,69 +28,67 @@
   (define-values (a b c d) (time-apply f null))
   c)
 
-(define (batch proc n hostname i)
+(define (batch proc n hostname)
   (let-values ([(in out) (tcp-connect hostname 8080)])
     (define t (proc n in out))
     (close-input-port in)
     (close-output-port out)
-    (display #\return)
-    (display i)
     t))
 
 (define (run-a-test proc hostname)
-  (define-values (b t) (find-size (lambda (x)  (with-time (lambda () (batch proc x hostname 0)))) 10 1 1))
-    (let ([n (round (/ 20000 t))])
-    (display n)
-    (display " batches of ")
-    (display b)
-    (displayln "kB")
-    (let* ([values (map (lambda (x) (batch proc b hostname x)) (range 0 n))]
-           [trimmed (trim values)]
-           [average (round (/ (foldr + 0 trimmed) (length trimmed)))]
-           [speed (exact->inexact (/ (round (/ (* 800 b) average)) 100))])
-      (displayln trimmed)
-      (display average)
-      (displayln " ms")
-      (display speed)
-      (displayln " Mbps")
-      ) ))
+  (define-values (b t) (find-size (lambda (x)  (with-time (lambda () (batch proc x hostname)))) 10 1 1)) 
+    (let* ([n (round (/ 20000 t))]
+           [values (map
+                    (lambda (x)
+                      (display #\return)
+                      (display (round (/ (* 100 (+ x 1)) n)))
+                      (display "%")
+                      (batch proc b hostname))
+                    (range 0 n))])
+           (report values b)
+           ))
 
+(define (report values b)
+  (let* ([trimmed (trim values)]
+         [average (round (/ (foldr + 0 trimmed) (length trimmed)))]
+         [speed (exact->inexact (/ (round (/ (* 800 b) average)) 100))])
+      (display #\newline)
+      (display speed)
+      (displayln " Mbps")))
 
 (define (run-test hostname)
   (displayln "Testing Download")
   (run-a-test request-download hostname)
   (displayln "Testing Upload")
-  (run-a-test request-upload hostname)
-  )
+  (run-a-test request-upload hostname))
 
 (define (trim values)
   (let* ([sorted (sort values <=)]
         [n (length values)]
         [front (round (/ n 10))]
         [back (round (/ n 5))])
-    (trim-list sorted front back)
-    ))
+    (trim-list sorted front back)))
 
 (define (trim-list list front back)
-  (list-tail (reverse (list-tail (reverse list) back)) front)
-  )
+  (list-tail (reverse (list-tail (reverse list) back)) front))
 
 ;finds the size of batches to test, returns n such that 50 < proc n < 200
+;want b < 20 000
+; t < 500
+; t > 50
 ;proc : n -> ms
 (define (find-size proc b c t1)
 (let ([t (proc b)])
-  (cond [(<= t 200) (find-size proc (round (* b (/ 200 t))) b t)]
+  (cond [(<= 20000 b) (values c t1)]
+        [(<= t 200) (find-size proc (round (* b (/ 200 t))) b t)]
         [(<= 1000 t) (values c t1)]
-        [else ((values b t) ])
-  ))
-
+        [else (values b t) ])))
 
 (define (speedcheck)
   (if (= (vector-length (current-command-line-arguments)) 1) 
       (let  ([hostname (vector-ref (current-command-line-arguments) 0)])
         (displayln hostname)
         (run-test hostname))
-      (displayln "Bad args")
-      ))
+      (displayln "Bad args")))
 
 (speedcheck)
