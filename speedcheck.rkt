f5f302fe3eed757413a5382e3cfbb9428d22d59f
Daniel Mroz <daniel.mroz@univ.ox.ac.uk>
2018-10-20 17:35:29 +0100

    cleaned up port passing

--- speedcheck.rkt
+++ speedcheck.rkt
@@ -2,56 +2,65 @@
 (require racket/tcp)
 (require racket/cmdline)
 
-; n : int, kB
-(define (upload n in out)
-  (write-bytes (bytes 1) out)
-  (flush-output)
-  (write-string (number->string n) out)
-  (newline out)
-  (flush-output out)
-  (time-expr (write-bytes (make-bytes (* 1000 n) 6) out)))
-
-; n : int, kB
-(define (download n in out)
-  (write-bytes (bytes 0) out)
-  (flush-output)
-  (write-string (number->string n) out)
-  (newline out)
-  (flush-output out)
-  (time-expr (read-bytes (* 1000 n) in)))
-
 (define-syntax-rule (time-expr e) 
   (let-values ([(a b c d) (time-apply (lambda () e) null)])
    c))
 
-(define (run-a-test proc in out)
-  (define-values (b t) (find-size (lambda (x)  (time-expr (proc x in out))) 10 1 1)) 
+(define (send-header x n out)
+  (write-bytes (bytes x) out)
+  (flush-output)
+  (write-string (number->string n) out)
+  (newline out)
+  (flush-output out))
+
+; n : int, kB
+(define (upload n in out)
+  (send-header 1 n out)
+  (time-expr (write-bytes (make-bytes (* 1000 n) 6) out)))
+
+; n : int, kB
+(define (download n in out)
+  (send-header 0 n out)
+  (time-expr (read-bytes (* 1000 n) in)))
+
+(define (run-a-test proc)
+  (define-values (b t) (find-size proc 10 1 1)) 
     (let* ([n (round (/ 20000 (max t 1)))]
            [values (map
                     (lambda (x)
-                      (display #\return)
-                      (display (round (/ (* 100 (+ x 1)) n)))
-                      (display "%")
-                      (proc b in out))
+                      (display-inline (percent x n))
+                      (proc b))
                     (range 0 n))])
            (report values b)
            ))
 
+(define (percent x y)
+  (string-append
+   (number->string (round (/ (* 100 (+ x 1)) y)))
+   "%"))
+  
+(define (display-inline x)
+  (display #\return)
+  (display x))
+
 (define (run-test hostname port)
   (define-values (in out) (tcp-connect hostname port))
   (displayln "Testing Download")
-  (run-a-test download in out)
+  (run-a-test (lambda (x) (download x in out)))
   (displayln "Testing Upload")
-  (run-a-test upload in out)
+  (run-a-test (lambda (x) (upload x in out)))
   (close-input-port in)
   (close-output-port out))
 
+(define (avg-list xs)
+  (/ (foldr + 0 xs) (length xs)))
+
 (define (report values b)
   (let* ([trimmed (trim values)]
-         [average (round (/ (foldr + 0 trimmed) (length trimmed)))]
-         [speed (exact->inexact (/ (round (/ (* 800 b) average)) 100))])
+         [avg-time (round (avg-list trimmed))]
+         [speed  (/ (* 8 b) avg-time)])
       (display #\newline)
-      (display speed)
+      (display (real->decimal-string speed))
       (displayln " Mbps")))
 
 (define (trim values)
@@ -59,9 +68,9 @@
          [n (length values)]
          [front (round (/ n 10))]
          [back (round (/ n 5))])
-    (trim-list sorted front back)))
+    (chop-list sorted front back)))
 
-(define (trim-list list front back)
+(define (chop-list list front back)
   (list-tail (reverse (list-tail (reverse list) back)) front))
 
 ;finds the size of batches to test
@@ -89,7 +98,9 @@
         ))
 
 (define (speedcheck)
- (cond [(get-args) => (lambda (x) (with-handlers ([exn:fail? (lambda (e) (displayln "\nUh oh, something went wrong"))]) (run-test (car x) (cdr x))))]
+ (cond [(get-args) => (lambda (x)
+                        (with-handlers ([exn:fail? (lambda (e) (displayln "\nUh oh, something went wrong"))])
+                          (run-test (car x) (cdr x))))]
        [else (displayln "Usage: 'speedcheck [hostname] [port]'. Port is optional, default is 8080")]))
 
 (speedcheck)
